77bf9cc9b75b716895d5bfce803734bc41b1c91a
Marcos Ribeiro <marcossrj90@gmail.com>
2020-06-13 18:50:13 -0300

    vinculando subgrupo no grupo de pericias

--- src/app/nova-ficha/nova-ficha.component.ts
+++ src/app/nova-ficha/nova-ficha.component.ts
@@ -16,7 +16,23 @@ export class NovaFichaComponent implements OnInit {
     var g = (<HTMLSelectElement>document.getElementById(grupo));
     var sg = (<HTMLSelectElement>document.getElementById(subgrupo));
 
-    
+    sg.innerHTML = "";
+
+    if(g.value == "idioma") {
+      var opcaoSub = ["portugues|Portugues","ingles|Ingles"];
+    } else if(g.value == "ciencias") {
+      var opcaoSub = ["herbalismo|Herbalismo","historia|História"];
+    } else if(g.value == "cproibidas") {
+      var opcaoSub = ["alquimia|Alquimia","ocultismo|Ocultismo"];
+    }
+
+    for(var opcao in opcaoSub) {
+      var par = opcaoSub[opcao].split("|");
+      var novaOpcao = <HTMLOptionElement>document.createElement("option");
+      novaOpcao.value = par[0];
+      novaOpcao.innerHTML = par[1];
+      sg.options.add(novaOpcao);
+    }
   }
 
 }
